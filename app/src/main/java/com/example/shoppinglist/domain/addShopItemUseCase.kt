d62b8a48ab57bf0d8ae10f52cf5e5345de679bd0
kstekels <k.stekels@gmail.com>
2021-08-09 11:06:40 +0300

    Create Domain layer

---
+++ app/src/main/java/com/example/shoppinglist/domain/addShopItemUseCase.kt
@@ -0,0 +1,7 @@
+package com.example.shoppinglist.domain
+
+class addShopItemUseCase(private val shopListRepository: ShopListRepository) {
+    fun addShopItem(shopItem: ShopItem) {
+        shopListRepository.addShopItem(shopItem)
+    }
+}
